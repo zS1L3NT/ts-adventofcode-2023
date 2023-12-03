3f6f2029c5bdf07415a30cc0e4be492b97a07356
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-12-03 14:05:25 +0800

    ✨ day 3

---
+++ src/day-3/part-1.ts
@@ -0,0 +1,41 @@
+import { readFileSync } from "fs"
+import { resolve } from "path"
+
+const input = readFileSync(resolve("src/day-3/actual.txt"), "utf-8").split("\n")
+
+let sum = 0
+
+for (let y = 0; y < input.length; y++) {
+	const n = input[y]!
+
+	const nums = [...n.matchAll(/(\d+)/g)].map(n => [n.index!, n.index! + n[0].length] as const)
+
+	for (const [start, end] of nums) {
+		const points = [
+			[start - 1, y - 1],
+			[start - 1, y],
+			[start - 1, y + 1],
+			...Array(end - start)
+				.fill(start)
+				.flatMap(
+					(x, i) =>
+						[
+							[x + i, y - 1],
+							[x + i, y + 1],
+						] as const,
+				),
+			[end, y - 1],
+			[end, y],
+			[end, y + 1],
+		]
+
+		for (const [x, y] of points) {
+			const c = input[y]?.[x]
+			if (c && !c.match(/[\d\\.]/)) {
+				sum += +n.slice(start, end)
+			}
+		}
+	}
+}
+
+console.log(sum)
---
+++ src/day-3/part-2.ts
@@ -0,0 +1,55 @@
+import { readFileSync } from "fs"
+import { resolve } from "path"
+
+const input = readFileSync(resolve("src/day-3/actual.txt"), "utf-8").split("\n")
+
+const matrix = [
+	[-1, -1],
+	[0, -1],
+	[1, -1],
+	[-1, 0],
+	[0, 0],
+	[1, 0],
+	[-1, 1],
+	[0, 1],
+	[1, 1],
+] as [number, number][]
+
+let sum = 0
+
+for (let y = 0; y < input.length; y++) {
+	const n = input[y]!
+
+	for (let x = 0; x < n.length; x++) {
+		if (input[y]?.[x] !== "*") continue
+
+		const points = matrix
+			.map(p => [p[0] + x, p[1] + y] as const)
+			.filter(p => input[p[1]]?.[p[0]]?.match(/\d/))
+		if (points.length < 2) continue
+
+		const numbers: { start: number; end: number; number: string }[] = []
+		for (const [x, y] of points) {
+			let start = x
+			let end = x
+
+			while (input[y]![start]?.match(/\d/)) {
+				start--
+			}
+
+			while (input[y]![end]?.match(/\d/)) {
+				end++
+			}
+
+			numbers.push({ start, end, number: input[y]!.slice(start + 1, end) })
+		}
+
+		// @ts-ignore
+		const unique = [...new Set(numbers.map(JSON.stringify))].map(JSON.parse) as typeof numbers
+		if (unique.length !== 2) continue
+
+		sum += +unique[0]!.number * +unique[1]!.number
+	}
+}
+
+console.log(sum)
