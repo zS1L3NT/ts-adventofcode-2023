d299f2d437ae4a7129df5842d0e2b0757c892cff
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-12-06 15:32:20 +0800

    ✨ day 6

---
+++ src/day-6/part-1.ts
@@ -0,0 +1,30 @@
+import file from "../file"
+
+const [times, distances] = file("actual").map(v =>
+	v
+		.split(":")[1]!
+		.trim()
+		.split(" ")
+		.filter(Boolean)
+		.map(v => +v),
+) as [number[], number[]]
+
+let ways = 1
+for (let i = 0; i < times.length; i++) {
+	const time = times[i]!
+	const distance = distances[i]!
+
+	let min = [Infinity, 0] as [number, number]
+	let max = [-Infinity, 0] as [number, number]
+	for (let j = 0; j <= time; j++) {
+		const n = j * (time - j)
+		if (n > distance) {
+			if (j < min[0]) min = [j, n]
+			if (j > max[0]) max = [j, n]
+		}
+	}
+
+	ways *= max[0] - min[0] + 1
+}
+
+console.log(ways)
